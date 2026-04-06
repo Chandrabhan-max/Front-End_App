e7f2da5dd39a355dda3016e495ee125e4754217f
Chandrabhan-max <getabhi299@gmail.com>
2026-04-06 23:39:27 +0530

    updated backend url to vercel

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:5000/api',
-  baseURL: 'https://back-end-server-nine.vercel.app/api',
+  baseURL: 'https://back-end-server-nine.vercel.app',
 });
 
 export default api;
